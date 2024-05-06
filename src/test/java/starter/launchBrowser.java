ad97074d7ae87e812a2692efc973ab9802e7018d
Immaculatekimani <immaculatewanjira21@gmail.com>
2024-05-06 07:35:40 +0300

    Launch Browser
    Implement launch browser with playwright

--- src/test/java/starter/launchBrowser.java
+++ src/test/java/starter/launchBrowser.java
@@ -1,4 +1,17 @@
 package starter;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
 public class launchBrowser {
+    public static void main(String[] args) {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions().setHeadless(false)
+        );
+        Page page = browser.newPage();
+        page.navigate("https://ecommerce-playground.lambdatest.io/");
+    }
 }
